f0c0bdaeeaf9d5c057c4e668e99494203b68af82
raajok <61615940+raajok@users.noreply.github.com>
2022-06-09 22:27:32 +0300

    Simple searchbar (#1)
    Co-authored-by: Rasmus Jokinen <raajok@utu.fi>

--- src/components/coins-data-display/CryptoDisplay.component.tsx
+++ src/components/coins-data-display/CryptoDisplay.component.tsx
@@ -5,6 +5,7 @@ import { Coin } from '../../models/coin.interface';
 
 const CryptoDisplay: React.FC = () => {
   const [coins, setcoins] = useState<Array<Coin>>([]);
+  const [filteredcoins, setfilteredcoins] = useState<Array<Coin>>([]);
   const [loading, setloading]: [boolean, (loading: boolean) => void] = useState<boolean>(true);
   const [error, seterror]: [string, (error: string) => void] = useState('');
 
@@ -12,14 +13,23 @@ const CryptoDisplay: React.FC = () => {
     // Fetch coindata and set to state
     CoinAPI.getCoins().then(data => {
       setcoins(data);
+      setfilteredcoins(data);
     });
   }, []);
-  console.log(coins);
+
+  // compares coins to the search input and sets the result to filteredcoins
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    let lowerCase = event.target.value.toLowerCase();
+    let searchedCoins = coins.filter(coin => coin.name.toLowerCase().includes(lowerCase));
+
+    setfilteredcoins(searchedCoins);
+  };
 
   return (
-    <div className='coinlist-wrapper'>
-      {loading && <Coinlist coinData={coins} />}
-    </div>
+      <div className='coinlist-wrapper'>
+        <input className='searchbar' onChange={handleSearch} placeholder="Search for a coin..." ></input> 
+        {loading && <Coinlist coinData={filteredcoins} />}
+      </div>
   );
 };
 
